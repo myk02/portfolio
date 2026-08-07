b0e888d9544c2460927357f3c7f61624c8edbe6c
Mike Waitindi <mikegary201@gmail.com>
2026-08-07 10:36:22 +0300

    chore: further reduce site-wide font size (14px to 13px base)

--- client/src/contexts/FontSizeContext.tsx
+++ client/src/contexts/FontSizeContext.tsx
@@ -29,7 +29,7 @@ function getInitialScale(): number {
   return 1;
 }
 
-const BASE_FONT_SIZE = 14;
+const BASE_FONT_SIZE = 13;
 
 function applyScale(scale: number) {
   const px = Math.round(BASE_FONT_SIZE * scale * 100) / 100;
